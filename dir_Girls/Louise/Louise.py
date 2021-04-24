41dc44a67d568322df9faf5a9c07b54d1598f779
Arfikait-Sirius <Arfikait.Sirius@gmail.com>
2021-04-25 01:56:34 +0900

    Learn Emily and Louise. - Emily#fn_disp_str(), Louise#fn_count(), Louise#fn_replace().

--- dir_Girls/Louise/Louise.py
+++ dir_Girls/Louise/Louise.py
@@ -1,11 +1,62 @@
-def fn_copy( base ):
+#------------------------
+# :[ NAME ]:
+#     fn_count
+#
+# :[ CATEGORY ]:
+#     Skill
+#------------------------
+def fn_count( base: str, target: str ) -> int:
+     return base.count( target )
+
+#------------------------
+# :[ NAME ]:
+#     fn_copy
+#
+# :[ CATEGORY ]:
+#     Skill
+#------------------------
+def fn_copy( base: str ) -> str:
      return base
 
-def fn_upper_all( base ):
+#------------------------
+# :[ NAME ]:
+#     fn_replace
+#
+# :[ CATEGORY ]:
+#     Skill
+#------------------------
+def fn_replace( base: str, target: str, replacement: str ) -> str:
+     return base.replace( target, replacement )
+
+#------------------------
+# :[ NAME ]:
+#     fn_upper_all
+#
+# :[ CATEGORY ]:
+#     Skill
+#------------------------
+def fn_upper_all( base: str ) -> str:
      return base.upper()
 
-def fn_lower_all( base ):
+#------------------------
+# :[ NAME ]:
+#     fn_lower_all
+#
+# :[ CATEGORY ]:
+#     Skill
+#------------------------
+def fn_lower_all( base: str ) -> str:
      return base.lower()
 
-def fn_upper_first( base ):
-     return base[0].upper() + base[1 : len( base )].lower()
+#------------------------
+# :[ NAME ]:
+#     fn_upper_first
+#
+# :[ CATEGORY ]:
+#     Skill
+#------------------------
+def fn_upper_first( base: str ) -> str:
+     base_upper: str = base.upper()[0]
+     base_lower: str = base.lower()[1 : len(base)]
+
+     return base_upper + base_lower
